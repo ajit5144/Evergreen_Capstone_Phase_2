ac7586f3fd2fad41f35f7f48a4ae3a274a37d671
Ajit SAhu <ajit.rsahu@gmail.com>
2026-08-31 19:38:59 +0000

    Commit 10 - Updating CI again

--- starter/vite.config.ts
+++ starter/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Evergreen-Quote-Capstone-Phase1/",
   server: {
     watch: {
       usePolling: true,
